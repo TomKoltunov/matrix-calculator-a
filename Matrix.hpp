a1cc40dadedaa969abfd12398cff1b3bdd87f877
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2022-04-05 16:20:13 +0300

    "Matrix.hpp" finished

--- Matrix.hpp
+++ Matrix.hpp
@@ -32,7 +32,19 @@ namespace zich
             bool operator==(const Matrix& mat) const;
             bool operator!=(const Matrix& mat) const;
 
+            Matrix& operator++();
+            Matrix& operator--();
+            Matrix operator++(int number);
+            Matrix operator--(int number);
+
+            
+
+            Matrix& operator*(const Matrix& other) const;
+
+            Matrix& operator*=(double number);
+
             friend ostream& operator<< (ostream& output, const Matrix& mat);
             friend istream& operator>> (istream& input , Matrix& mat);
+            friend Matrix operator*(double number, const Matrix& mat);
     };
 }
